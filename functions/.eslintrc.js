358d6dd48c2979319ef5d176baa7e165c8434106
Giorgio Garasto <giorgio@garasto.me>
2021-03-29 20:32:38 +0200

    chore: setup monorepo structure with lerna

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,27 +1,14 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'google',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: '../.eslintrc',
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.dev.json'],
-    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  ignorePatterns: [
-    '/lib/**/*', // Ignore built files.
-  ],
-  plugins: ['@typescript-eslint', 'import'],
+  env: {
+    node: true,
+    commonjs: true,
+  },
   rules: {
-    quotes: ['error', 'double'],
+    'no-console': 'off',
   },
 };
