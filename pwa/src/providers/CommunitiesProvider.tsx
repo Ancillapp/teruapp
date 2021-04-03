15901bef1f66ba28fb0818d55153b2b390d47814
Giorgio Garasto <giorgio@garasto.me>
2021-04-03 10:10:00 +0200

    feat: add selected community songbooks to communities provider

--- pwa/src/providers/CommunitiesProvider.tsx
+++ pwa/src/providers/CommunitiesProvider.tsx
@@ -6,13 +6,16 @@ import React, {
   useEffect,
 } from 'react';
 
-import useCommunitiesQuery from '../hooks/useCommunitiesQuery';
+import useCommunitiesQuery from '../hooks/data/useCommunitiesQuery';
+import useSongBooksLazyQuery from '../hooks/data/useSongBooksLazyQuery';
 import { Community } from '../models/community';
+import { SongBookSummary } from '../models/songBook';
 
 export const CommunitiesContext = createContext<
   | {
       communities?: Community[];
       selectedCommunity?: Community | null;
+      selectedCommunitySongBooks?: SongBookSummary[];
       setSelectedCommunity(newCommunity: Community): void;
     }
   | undefined
@@ -21,9 +24,15 @@ export const CommunitiesContext = createContext<
 export const CommunitiesProvider: FunctionComponent = ({ children }) => {
   const { loading, data } = useCommunitiesQuery();
 
+  const [getSongBooks] = useSongBooksLazyQuery();
+
   // TODO: store this value in indexedDB
   const [selectedCommunity, setSelectedCommunity] = useState<
     Community | null | undefined
+  >();
+
+  const [selectedCommunitySongBooks, setSelectedCommunitySongBooks] = useState<
+    SongBookSummary[] | undefined
   >();
 
   // This is just to simulate asynchronously getting the variable from IDB
@@ -31,12 +40,26 @@ export const CommunitiesProvider: FunctionComponent = ({ children }) => {
     setSelectedCommunity(null);
   }, []);
 
+  useEffect(() => {
+    const updateSongBooks = async () => {
+      if (selectedCommunity) {
+        const songBooks = await getSongBooks({
+          community: selectedCommunity.id,
+        });
+        setSelectedCommunitySongBooks(songBooks);
+      }
+    };
+
+    updateSongBooks();
+  }, [selectedCommunity]);
+
   return (
     <CommunitiesContext.Provider
       value={{
         communities: loading || !data ? undefined : data,
         selectedCommunity,
         setSelectedCommunity,
+        selectedCommunitySongBooks,
       }}
     >
       {children}
