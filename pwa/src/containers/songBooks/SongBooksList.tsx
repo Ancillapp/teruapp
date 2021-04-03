c0c077fa0e99c326f8d1e152df25fe926594e20e
Giorgio Garasto <giorgio@garasto.me>
2021-04-03 10:10:48 +0200

    feat: use songbooks from provider

--- pwa/src/containers/songBooks/SongBooksList.tsx
+++ pwa/src/containers/songBooks/SongBooksList.tsx
@@ -4,25 +4,20 @@ import { Redirect, useRouteMatch } from 'react-router-dom';
 
 import TopbarLayout from '../../components/common/TopbarLayout';
 import Loader from '../../components/common/Loader';
-import useSongBooksQuery from '../../hooks/useSongBooksQuery';
 import { useCommunities } from '../../providers/CommunitiesProvider';
 import PageSkeleton from '../../components/common/PageSkeleton';
 
 const SongBooksList: FunctionComponent = () => {
   const { path } = useRouteMatch();
 
-  const { selectedCommunity } = useCommunities();
+  const { selectedCommunitySongBooks } = useCommunities();
 
-  const { loading, data } = useSongBooksQuery({
-    community: selectedCommunity?.id,
-  });
-
-  if (loading || !data) {
+  if (!selectedCommunitySongBooks) {
     return <PageSkeleton />;
   }
 
-  return data.length === 1 ? (
-    <Redirect to={`${path}/${data[0].id}/canti`} />
+  return selectedCommunitySongBooks.length === 1 ? (
+    <Redirect to={`${path}/${selectedCommunitySongBooks[0].id}/canti`} />
   ) : (
     <TopbarLayout title="Libri dei canti">
       <Loader />
