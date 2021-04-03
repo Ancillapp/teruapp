08d3555baa4e68a94969e67e9d41162f78203766
Giorgio Garasto <giorgio@garasto.me>
2021-04-03 10:09:41 +0200

    feat: implement useSongBooksLazyQuery hook

---
+++ pwa/src/hooks/data/useSongBooksLazyQuery.ts
@@ -0,0 +1,40 @@
+import { useCallback } from 'react';
+import { SongBookSummary } from '../../models/songBook';
+
+import { useLazyQuery, UseLazyQueryResult } from '../fetching';
+
+export interface SongBooksLazyQueryParams {
+  community?: string;
+}
+
+export type SongBooksLazyQueryFunction = (
+  params?: SongBooksLazyQueryParams,
+) => Promise<SongBookSummary[]>;
+
+const useSongBooksLazyQuery = (): [
+  SongBooksLazyQueryFunction,
+  UseLazyQueryResult<SongBookSummary[]>,
+] => {
+  const [getSongBooks, data] = useLazyQuery<
+    SongBookSummary[],
+    undefined,
+    undefined,
+    SongBooksLazyQueryParams
+  >('songbooks');
+
+  const query = useCallback<SongBooksLazyQueryFunction>(
+    async ({ community } = {}) =>
+      getSongBooks(
+        community
+          ? {
+              query: { community },
+            }
+          : undefined,
+      ),
+    [],
+  );
+
+  return [query, data];
+};
+
+export default useSongBooksLazyQuery;
