2b885a1d79942fe7c5714aa53a0c1b7ff794852f
Giorgio Garasto <giorgio@garasto.me>
2021-04-03 13:55:27 +0200

    chore: add published field to models

--- functions/src/models/mongo.ts
+++ functions/src/models/mongo.ts
@@ -11,12 +11,13 @@ export interface Song extends MongoDBRecord {
   category: string;
   songBooks: {
     id: ObjectId;
+    number: string;
     // We replicate this information here because the number of reads
     // is much higher than the number of writes, so it is better to
     // replicate the information and update it when needed instead of
     // having to do some joins when reading the data
     title: string;
-    number: string;
+    published?: boolean;
   }[];
 }
 
@@ -33,5 +34,6 @@ export interface Community extends MongoDBRecord {
     // replicate the information and update it when needed instead of
     // having to do some joins when reading the data
     title: string;
+    published?: boolean;
   }[];
 }
