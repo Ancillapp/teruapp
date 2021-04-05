5bb8f2497fd15e4c891f44411f21d7e3ad541be3
Giorgio Garasto <giorgio@garasto.me>
2021-04-05 11:08:44 +0200

    feat: implement cache and network approach for communities

--- pwa/src/helpers/db.ts
+++ pwa/src/helpers/db.ts
@@ -1,7 +1,9 @@
 import Dexie, { Table } from 'dexie';
+import { Community } from '../models/community';
 
 class TeruappDB extends Dexie {
   settings: Table<{ key: string; value: unknown }, string>;
+  communities: Table<Community, string>;
 
   constructor() {
     super('teruapp');
@@ -10,9 +12,11 @@ class TeruappDB extends Dexie {
     // so we set the version number divided by 10
     this.version(0.1).stores({
       settings: 'key',
+      communities: 'id',
     });
 
     this.settings = this.table('settings');
+    this.communities = this.table('communities');
   }
 }
 
--- pwa/src/hooks/data/useCommunitiesQuery.ts
+++ pwa/src/hooks/data/useCommunitiesQuery.ts
@@ -1,7 +1,33 @@
+import { useLiveQuery } from 'dexie-react-hooks';
+
 import { Community } from '../../models/community';
+import { db } from '../../helpers/db';
 
 import { useQuery } from '../fetching';
+import { useEffect } from 'react';
 
-const useCommunitiesQuery = () => useQuery<Community[]>('communities');
+const useCommunitiesQuery = () => {
+  const communities = useLiveQuery(() => db.communities.toArray());
+
+  const { data, ...rest } = useQuery<Community[]>('communities');
+
+  useEffect(() => {
+    if (data) {
+      if (communities) {
+        const deletedCommunities = communities.filter((community) =>
+          data.every(({ id }) => id !== community.id),
+        );
+
+        db.communities.bulkDelete(deletedCommunities.map(({ id }) => id));
+      }
+
+      db.communities.bulkPut(data);
+    }
+    // We don't want to add communities to the dependencies to avoid infinite loops
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return { data: communities, ...rest };
+};
 
 export default useCommunitiesQuery;
