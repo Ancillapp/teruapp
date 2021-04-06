10777c9c10830a07a1545651171336e515072050
Giorgio Garasto <giorgio@garasto.me>
2021-04-06 16:12:59 +0200

    feat: add top bar start adornment placeholder on page skeleton

--- pwa/src/components/common/PageSkeleton.tsx
+++ pwa/src/components/common/PageSkeleton.tsx
@@ -1,13 +1,20 @@
 import React, { FunctionComponent } from 'react';
 
-import { Skeleton } from '@material-ui/core';
+import { IconButton, Skeleton } from '@material-ui/core';
 
 import TopbarLayout from './TopbarLayout';
+import TopbarIcon from './TopbarIcon';
 import Loader from './Loader';
 
 const PageSkeleton: FunctionComponent = () => (
   <TopbarLayout
-    startAdornment={null}
+    startAdornment={
+      <TopbarIcon sx={{ mr: 0.5 }}>
+        <IconButton edge="start" disabled>
+          <Skeleton variant="circular" width={24} height={24} />
+        </IconButton>
+      </TopbarIcon>
+    }
     title={<Skeleton variant="text" width={192} />}
   >
     <Loader />
