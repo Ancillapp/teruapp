d49ed70aecf1af75260f5df5590454ad8871b07b
Giorgio Garasto <giorgio@garasto.me>
2021-04-08 08:55:09 +0200

    feat: implement custom upsert and bulk upsert methods

--- pwa/src/helpers/db.ts
+++ pwa/src/helpers/db.ts
@@ -1,4 +1,4 @@
-import Dexie, { Table } from 'dexie';
+import Dexie, { IndexableType, PromiseExtended, Table } from 'dexie';
 import { Community } from '../models/community';
 import { SongBookSummary } from '../models/songBook';
 import { Song } from '../models/song';
@@ -8,10 +8,14 @@ export interface DBSetting {
   value: unknown;
 }
 
-export type DBCommunity = Community;
+export interface DBCommunity extends Community {
+  songBooks?: {
+    id: string;
+  }[];
+}
 
 export interface DBSongBook extends SongBookSummary {
-  songs: {
+  songs?: {
     id: string;
     number: string;
   }[];
@@ -21,11 +25,20 @@ export interface DBSong extends Omit<Song, 'content'> {
   content?: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface TeruappTable<T = any, TKey = IndexableType>
+  extends Table<T, TKey> {
+  upsert(key: TKey, item: T): PromiseExtended<TKey>;
+  bulkUpsert(keys: TKey[], items: readonly T[]): PromiseExtended<TKey>;
+}
+
 class TeruappDB extends Dexie {
-  settings: Table<DBSetting, string>;
-  communities: Table<DBCommunity, string>;
-  songBooks: Table<DBSongBook, string>;
-  songs: Table<DBSong, string>;
+  declare readonly tables: TeruappTable[];
+
+  settings: TeruappTable<DBSetting, string>;
+  communities: TeruappTable<DBCommunity, string>;
+  songBooks: TeruappTable<DBSongBook, string>;
+  songs: TeruappTable<DBSong, string>;
 
   constructor() {
     super('teruapp');
@@ -43,6 +56,33 @@ class TeruappDB extends Dexie {
     this.communities = this.table('communities');
     this.songBooks = this.table('songBooks');
     this.songs = this.table('songs');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  table<T = any, TKey = IndexableType>(
+    tableName: string,
+  ): TeruappTable<T, TKey> {
+    const table = super.table(tableName) as TeruappTable<T, TKey>;
+
+    table.upsert = (key, item) =>
+      table.get(key).then((value) =>
+        table.put({
+          ...value,
+          ...item,
+        }),
+      );
+
+    table.bulkUpsert = (keys, items) =>
+      table.bulkGet(keys).then((values) =>
+        table.bulkPut(
+          values.map((value, index) => ({
+            ...value,
+            ...items[index],
+          })),
+        ),
+      );
+
+    return table;
   }
 }
 
